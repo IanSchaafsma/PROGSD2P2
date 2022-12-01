146e1ca9aac3e4a7982b5751da6c4a753a629201
Ian <33319@ma-web.nl>
2022-12-01 12:05:49 +0100

    week 2 opdracht 2

--- PROGCMS-week1/private/init.php
+++ PROGCMS-week1/private/init.php
@@ -2,4 +2,17 @@
 include "../../PROGCMS-week1";
 include "../private";
 include "../private/shared";
+
+$root = substr($_SERVER['SCRIPT_NAME'],
+    0,
+    strpos($_SERVER['SCRIPT_NAME'], '/public') + 7);
+define("ROOT_URL", $root);
+
+function url_path($path) {
+    if($path[0] != '/') {
+        $path = "/" . $path;
+    }
+    return ROOT_URL . $path;
+}
+
 ?>
---
+++ PROGCMS-week1/public/contentbeheer/index.php
@@ -0,0 +1,30 @@
+<?php
+$url = "../../private/shared/header.php";
+include $url;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Startpagina</title>
+    <link rel="stylesheet" href="public/styles/style.css">
+</head>
+<body>
+<header>
+  Welkom
+</header>
+
+<h1>
+  Standaard tekst
+</h1>
+
+<footer>
+
+</footer>
+  Privacy verklaring
+</body>
+<?php
+include "private/shared/footer.php";
+?>
+</html>
